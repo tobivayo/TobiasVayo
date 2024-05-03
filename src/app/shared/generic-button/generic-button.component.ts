2aeb20ed4582f518e7d51cc15ad4ffc91b40e027
Tobias Vayo <tobivayo@gmail.com>
2024-05-03 17:52:35 -0300

    initial commit

---
+++ src/app/shared/generic-button/generic-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './generic-button.component.html',
+  styleUrl: './generic-button.component.css'
+})
+export class GenericButtonComponent {
+
+}
